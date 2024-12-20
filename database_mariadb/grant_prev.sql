3f75de9e6f773fc55ad3a69c15b24bca758601a9
vinayakmurthy <vinayakarmurthy@gmail.com>
2024-12-20 19:13:54 +0530

    final-changes-done-dusted

--- database_mariadb/grant_prev.sql
+++ database_mariadb/grant_prev.sql
@@ -1,5 +1,7 @@
 
 CREATE USER IF NOT EXISTS 'vinayak'@'%';
 GRANT ALL PRIVILEGES ON foodogram.* TO 'vinayak'@'%';
+
+CREATE USER IF NOT EXISTS 'vinayak'@'localhost';
 GRANT ALL PRIVILEGES ON foodogram.* TO 'vinayak'@'localhost';
 FLUSH PRIVILEGES;
