22b9a77ff0c3f80700d8e355ae912d79103b2d29
vinayakmurthy <vinayakarmurthy@gmail.com>
2024-12-21 16:42:33 +0530

    i think this will pakka work

--- database_mariadb/initdb.sh
+++ database_mariadb/initdb.sh
@@ -2,10 +2,10 @@
 
 # Generate and execute the SQL commands for user creation and granting privileges
 cat <<EOF > /tmp/grant_prev.sql
-CREATE USER IF NOT EXISTS 'vinayak'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+CREATE USER IF NOT EXISTS 'vinayak'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON foodogram.* TO 'vinayak'@'%';
 
-CREATE USER IF NOT EXISTS 'vinayak'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+CREATE USER IF NOT EXISTS 'vinayak'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON foodogram.* TO 'vinayak'@'localhost';
 FLUSH PRIVILEGES;
 EOF
